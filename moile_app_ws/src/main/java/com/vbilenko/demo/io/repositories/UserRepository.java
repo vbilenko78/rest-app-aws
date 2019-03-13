9c4d770ed991749dc9d13ee8c84a87fae3a12c73
Vladimir Bilenko <vbilenko@yahoo.com>
2019-03-12 22:45:57 -0700

    moved UserRepository file to repo package

--- moile_app_ws/src/main/java/com/vbilenko/demo/io/repositories/UserRepository.java
+++ moile_app_ws/src/main/java/com/vbilenko/demo/io/repositories/UserRepository.java
@@ -1,4 +1,4 @@
-package com.vbilenko.demo;
+package com.vbilenko.demo.io.repositories;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
